42492e7a5fcf3fa4c11a37a4e30da7f89faa45cf
themoonr4 <themoonr4@gmail.com>
2026-04-24 00:25:20 +0530

    Add Manifest.js for THE MOON News app

---
+++ Manifest.js
@@ -0,0 +1,10 @@
+{
+  "name": "THE MOON News",
+  "short_name": "THE MOON",
+  "description": "World Class News Platform",
+  "start_url": "/",
+  "display": "standalone",
+  "theme_color": "#c00000",
+  "background_color": "#ffffff",
+  "icons": [{ "src": "assets/logo.svg", "sizes": "192x192", "type": "image/svg+xml" }]
+}
